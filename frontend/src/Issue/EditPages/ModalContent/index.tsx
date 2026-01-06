8d038e12cee116e1718786b8d121f0e8e4a54320
matt1432 <matt@nelim.org>
2026-01-06 16:29:24 -0500

    fix(react): allow editing page names manually

--- frontend/src/Issue/EditPages/ModalContent/index.tsx
+++ frontend/src/Issue/EditPages/ModalContent/index.tsx
@@ -331,6 +331,8 @@ export default function EditPagesModalContent({
                     ...newThumbnails[index],
                     newFilename: value,
                 };
+
+                setChanges(newThumbnails);
             },
         [changes],
     );
